322db9c3ce6ed37238f48ace4ed09ef790336df1
Dat Tran <dat.tran@kyanon.digital>
2025-03-03 13:45:35 +0700

    fix Plugins

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,7 @@
-export default () => ({});
+module.exports = ({ env }) => ({
+  "users-permissions": {
+    config: {
+      jwtSecret: env("JWT_SECRET", "yourDefaultSecretHere"),
+    },
+  },
+});
