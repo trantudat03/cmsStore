29500f716adf8c2d4d152d6b135193d1a75764cf
Dat Tran <dat.tran@kyanon.digital>
2025-03-03 21:31:21 +0700

    fix database host

--- config/database.ts
+++ config/database.ts
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
+      host: env("DATABASE_HOST", "postgres.railway.internal"),
       port: env.int("DATABASE_PORT", 5432),
       database: env("DATABASE_NAME", "strapi"),
       user: env("DATABASE_USERNAME", "strapi"),
