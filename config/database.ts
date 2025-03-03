5bb121e638eda4d213f37f895038c1a056f75bad
Dat Tran <dat.tran@kyanon.digital>
2025-03-03 14:49:54 +0700

    update db

--- config/database.ts
+++ config/database.ts
@@ -2,16 +2,15 @@ export default ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      connectionString: env("DATABASE_URL"),
-      ssl: env.bool("DATABASE_SSL", false) && {
-        rejectUnauthorized: env.bool("DATABASE_SSL_REJECT_UNAUTHORIZED", true),
-      },
-      schema: env("DATABASE_SCHEMA", "public"),
+      host: env("DATABASE_HOST", "localhost"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "railway"),
+      user: env("DATABASE_USERNAME", "postgres"),
+      password: env("DATABASE_PASSWORD", "mIZJLnhNWsssbtIKBuNWzSkJThvcXRku"),
+      ssl: env.bool("DATABASE_SSL", false)
+        ? { rejectUnauthorized: false }
+        : false,
     },
-    pool: {
-      min: env.int("DATABASE_POOL_MIN", 2),
-      max: env.int("DATABASE_POOL_MAX", 10),
-    },
-    acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
+    pool: { min: 2, max: 10 },
   },
 });
