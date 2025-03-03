367eab15609922ecf3d1c03b01a3e838ee2669d3
Dat Tran <dat.tran@kyanon.digital>
2025-03-03 21:40:41 +0700

    fix

--- config/database.ts
+++ config/database.ts
@@ -4,9 +4,9 @@ module.exports = ({ env }) => ({
     connection: {
       host: env("DATABASE_HOST", "postgres.railway.internal"),
       port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "strapi"),
-      user: env("DATABASE_USERNAME", "strapi"),
-      password: env("DATABASE_PASSWORD", "strapi"),
+      database: env("DATABASE_NAME", "railway"),
+      user: env("DATABASE_USERNAME", "postgres"),
+      password: env("DATABASE_PASSWORD", "mIZJLnhNWsssbtIKBuNWzSkJThvcXRku"),
       schema: env("DATABASE_SCHEMA", "public"), // Not required
       ssl: {
         rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
