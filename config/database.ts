fca6f9eae4be26eea8ca1de7b43b236e1ee43641
Dat Tran <dat.tran@kyanon.digital>
2025-03-03 16:00:59 +0700

    fig bug deploy

--- config/database.ts
+++ config/database.ts
@@ -1,21 +1,13 @@
 export default ({ env }) => ({
   connection: {
     client: "postgres",
-    connection: env("DATABASE_URL") // Sử dụng URL thay vì từng biến riêng lẻ
-      ? env("DATABASE_URL")
-      : {
-          host: env("DATABASE_HOST", "localhost"),
-          port: env.int("DATABASE_PORT", 5432),
-          database: env("DATABASE_NAME", "railway"),
-          user: env("DATABASE_USERNAME", "postgres"),
-          password: env(
-            "DATABASE_PASSWORD",
-            "mIZJLnhNWsssbtIKBuNWzSkJThvcXRku"
-          ),
-          ssl: env.bool("DATABASE_SSL", false)
-            ? { rejectUnauthorized: false }
-            : false,
-        },
+    connection: {
+      connectionString: env("DATABASE_URL"),
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
     pool: { min: 2, max: 10 },
+    debug: false,
   },
 });
