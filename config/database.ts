bda8e64644ffc5418e9d700117baa1f35a852c08
Dat Tran <dat.tran@kyanon.digital>
2025-03-03 15:39:52 +0700

    local connect success

--- config/database.ts
+++ config/database.ts
@@ -1,16 +1,21 @@
 export default ({ env }) => ({
   connection: {
     client: "postgres",
-    connection: {
-      host: env("DATABASE_HOST", "localhost"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "railway"),
-      user: env("DATABASE_USERNAME", "postgres"),
-      password: env("DATABASE_PASSWORD", "mIZJLnhNWsssbtIKBuNWzSkJThvcXRku"),
-      ssl: env.bool("DATABASE_SSL", false)
-        ? { rejectUnauthorized: false }
-        : false,
-    },
+    connection: env("DATABASE_URL") // Sử dụng URL thay vì từng biến riêng lẻ
+      ? env("DATABASE_URL")
+      : {
+          host: env("DATABASE_HOST", "localhost"),
+          port: env.int("DATABASE_PORT", 5432),
+          database: env("DATABASE_NAME", "railway"),
+          user: env("DATABASE_USERNAME", "postgres"),
+          password: env(
+            "DATABASE_PASSWORD",
+            "mIZJLnhNWsssbtIKBuNWzSkJThvcXRku"
+          ),
+          ssl: env.bool("DATABASE_SSL", false)
+            ? { rejectUnauthorized: false }
+            : false,
+        },
     pool: { min: 2, max: 10 },
   },
 });
